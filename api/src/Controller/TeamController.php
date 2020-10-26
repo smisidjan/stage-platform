70587b662ac6122ea7a5f4ed51306355e9ca4aea
Ruben van der Linde <ruben@conduction.nl>
2020-10-26 08:17:52 +0100

    Split user and organisation dashboard

---
+++ api/src/Controller/TeamController.php
@@ -0,0 +1,43 @@
+<?php
+
+// src/Controller/TeamController.php
+
+namespace App\Controller;
+
+//use App\Service\RequestService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * The TeamController test handles any calls that have not been picked up by another test, and wel try to handle the slug based against the wrc.
+ *
+ * Class TeamController
+ *
+ * @Route("/teams")
+ */
+class TeamController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @Template
+     */
+    public function indexAction()
+    {
+        $variables = [];
+
+        return $variables;
+    }
+
+    /**
+     * @Route("/teams/{id}")
+     * @Template
+     */
+    public function teamssAction()
+    {
+        $variables = [];
+
+        return $variables;
+    }
+
+}
