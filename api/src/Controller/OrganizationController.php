2370de0dee05fa1d4c23385b64a9eb7b6393a26a
Barry Brands <barrybrands02@hotmail.com>
2020-11-02 16:41:46 +0100

    Added organizations pages

---
+++ api/src/Controller/OrganizationController.php
@@ -0,0 +1,55 @@
+<?php
+
+// src/Controller/DefaultController.php
+
+namespace App\Controller;
+
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * The Procces test handles any calls that have not been picked up by another test, and wel try to handle the slug based against the wrc.
+ *
+ * Class DefaultController
+ *
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @Template
+     */
+    public function indexAction(CommonGroundService $commonGroundService, Request $request)
+    {
+        // On an index route we might want to filter based on user input
+        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
+
+        return $variables;
+    }
+
+    /**
+     * @Route("/login")
+     * @Template
+     */
+    public function loginAction(CommonGroundService $commonGroundService, Request $request)
+    {
+        $variables = [];
+
+        return $variables;
+    }
+
+    /**
+     * @Route("/register")
+     * @Template
+     */
+    public function registerAction(CommonGroundService $commonGroundService, Request $request)
+    {
+        $variables = [];
+
+        return $variables;
+    }
+}
