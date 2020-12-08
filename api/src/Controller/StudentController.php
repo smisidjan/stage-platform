8ca5fad8da1064c8087c56df1b1e8846f50797bf
Sarai Misidjan <smisidjan@roc-dev.com>
2020-12-08 11:46:06 +0100

    sender toegevoegd

--- api/src/Controller/StudentController.php
+++ api/src/Controller/StudentController.php
@@ -26,9 +26,9 @@ class StudentController extends AbstractController
     public function indexAction(CommonGroundService $commonGroundService, Request $request)
     {
         if (!$this->getUser() && $request->query->get('organization')) {
-            return $this->redirect($this->generateUrl('app_user_idvault').'?backUrl='.$request->getUri());
+            return $this->redirect($this->generateUrl('app_user_idvault') . '?backUrl=' . $request->getUri());
         } elseif ($this->getUser() && $request->query->get('organization') && empty($this->getUser()->getOrganization())) {
-            return $this->redirect($this->generateUrl('app_default_organization').'?backUrl='.$request->getUri());
+            return $this->redirect($this->generateUrl('app_default_organization') . '?backUrl=' . $request->getUri());
         }
 
         // On an index route we might want to filter based on user input
@@ -76,6 +76,10 @@ class StudentController extends AbstractController
         $variables['student'] = $commonGroundService->getResource(['component' => 'cc', 'type' => 'people', 'id' => $id]);
         $variables['participants'] = $commonGroundService->getResourceList(['component' => 'edu', 'type' => 'participants'], ['person' => $variables['student']['@id']])['hydra:member'];
 
+        if ($this->getUser()) {
+            $variables['sender'] = ($commonGroundService->isResource($this->getUser()->getPerson()));
+        }
+
         $variables['portfolio'] = $commonGroundService->getResourceList(['component' => 'pfc', 'type' => 'portfolios'], ['owner' => $variables['student']['@id']])['hydra:member'];
         if (isset($variables['portfolio']) && $variables['portfolio']) {
             $variables['portfolio'] = $variables['portfolio'][0];
@@ -114,7 +118,7 @@ class StudentController extends AbstractController
 
             if (isset($resource['contactMoment']) && $resource['contactMoment']) {
                 $resource['contactMoment']['receiver'] = $variables['student']['@id'];
-                $resource['contactMoment']['sender'] = $variables['student']['@id'];
+                $resource['contactMoment']['sender'] = $variables['sender']['@id'];
                 $resource['contactMoment']['channel'] = 'Direct message';
                 $resource['contactMoment']['topic'] = 'Direct message';
             }
